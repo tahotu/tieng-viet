1cec4975a847be2059669e33ada9a284bfc4f607
tahotu <anhlahau.hl+github2@gmail.com>
2024-06-29 23:57:57 +0900

    Add samples

---
+++ vizzuf/tao_html.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var files = fs.readdirSync('.');
+var mdFiles = files.filter(it => it.endsWith(".md"))
+
+const templateFilePath = "template.html"
+const templateContent = fs.readFileSync(templateFilePath).toString();
+
+mdFiles.forEach(mdFile => {
+    const mdContent = fs.readFileSync(mdFile).toString();
+    const sourceContentStart = mdContent.indexOf("\n") + 1;
+    const transPathStart = mdContent.indexOf("###", 10);
+    const transContentStart = mdContent.indexOf("\n", transPathStart) + 1;
+
+    const generatedContent = templateContent
+        .replace("$source", mdContent.slice(sourceContentStart, transPathStart))
+        .replace("$trans", mdContent.slice(transContentStart));
+    
+    const htmlFilePath = mdFile.slice(0, -2) + "html";
+    fs.writeFileSync(htmlFilePath, generatedContent);
+    console.log("Đã tạo file: ", htmlFilePath)
+})
